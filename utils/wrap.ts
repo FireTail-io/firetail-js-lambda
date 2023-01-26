1f458c082a16847b17ca6f7600dfe7e35fc65566
brian <brian@firetail.io>
2023-01-26 16:20:27 +0000

    Combined logic into 1 file

--- utils/wrap.ts
+++ utils/wrap.ts
@@ -1,10 +1,37 @@
+"use strict";
 import {
   APIGatewayProxyEvent,
   APIGatewayProxyEventV2,
   APIGatewayProxyResult,
   Context } from 'aws-lambda';
 
-import * as log from './log'
+  type ExecutionContent = {
+    statusCode:Number|String,
+    resBody:Object|String,
+    startedAt:Date,
+    finishedAt:Date
+  }
+
+  function log(event:APIGatewayProxyEvent|APIGatewayProxyEventV2, executionContent:ExecutionContent){
+    const date1_ms = executionContent.startedAt.getTime();
+    const date2_ms = executionContent.finishedAt.getTime();
+
+    // Calculate the difference in milliseconds
+    const executionTime = date2_ms - date1_ms;
+
+    const logExt = {
+      event,
+      response: {
+          statusCode:executionContent.statusCode,
+          body:executionContent.resBody
+      },
+      executionTime
+    }
+    //console.log("firetail:log-ext:",logExt)
+    console.log("firetail:log-ext:"+Buffer.from(JSON.stringify(logExt)).toString('base64'))
+
+  } // END log
+
 
 function wrap(next:Function){
 
