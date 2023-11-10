b329aebc743c45820fcab9dc58d302ab7a12b666
Timo Rüppell <timo.ruppell@gmail.com>
2023-11-10 13:57:14 +0200

    Convert indentation

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,52 +1,58 @@
-const Serverless_Events = require('./sampleEvents.json')
-const data = require('./animals.json')
+const Serverless_Events = require("./sampleEvents.json");
+const data = require("./animals.json");
 const firetailWrapper = require("../dist");
 
 //=====================================================
 //====================================== test GET calls
 //=====================================================
 
-describe('test Firetail:Serverless', () => {
+describe("test Firetail:Serverless", () => {
+    test("should work with lambda function url", done => {
+        const next = firetailWrapper(event => {
+            const statusCode = 200;
+            if (
+                event.queryStringParameters &&
+                event.queryStringParameters.limit
+            ) {
+                return {
+                    statusCode,
+                    body: JSON.stringify(
+                        data.slice(0, event.queryStringParameters.limit),
+                    ),
+                };
+            }
+            return {
+                statusCode,
+                body: JSON.stringify(data),
+            };
+        });
 
-    test('should work with lambda function url', done => {
+        const cLog = console.log;
 
-      const next = firetailWrapper((event) => {
-        const statusCode = 200
-        if(event.queryStringParameters
-        && event.queryStringParameters.limit){
-          return {
-            statusCode,
-            body: JSON.stringify(data.slice(0, event.queryStringParameters.limit)),
-          };
-        }
-        return {
-          statusCode,
-          body: JSON.stringify(data),
+        console.log = txt => {
+            expect(txt.startsWith("firetail:log-ext:")).toBe(true);
         };
-      });
-
-      const cLog = console.log
-
-      console.log = (txt)=>{
-        expect(txt.startsWith("firetail:log-ext:")).toBe(true);
-      }
-      next(Serverless_Events["lambda function url"])
-      .then((a)=>{
-        const  {statusCode,body} = a
-        expect(statusCode).toBe(200);
-        expect(body).toBe('[{"id":1,"name":"Bubbles","tag":"fish"},'+
-                           '{"id":2,"name":"Jax","tag":"cat"},'+
-                           '{"id":3,"name":"Tiger Lily","tag":"cat"},'+
-                           '{"id":4,"name":"Buzz","tag":"dog"},'+
-                           '{"id":5,"name":"Duke","owner":"Tom"}]')
-        return next(Serverless_Events["api Gateway Proxy Event"])
-      }).then(({statusCode,body})=>{
-          expect(statusCode).toBe(200);
-          expect(body).toBe('[{"id":1,"name":"Bubbles","tag":"fish"},'+
-                             '{"id":2,"name":"Jax","tag":"cat"}]')
-        console.log = cLog.bind(console)
-        done()
-      })
-
-    })
-})
+        next(Serverless_Events["lambda function url"])
+            .then(a => {
+                const { statusCode, body } = a;
+                expect(statusCode).toBe(200);
+                expect(body).toBe(
+                    '[{"id":1,"name":"Bubbles","tag":"fish"},' +
+                        '{"id":2,"name":"Jax","tag":"cat"},' +
+                        '{"id":3,"name":"Tiger Lily","tag":"cat"},' +
+                        '{"id":4,"name":"Buzz","tag":"dog"},' +
+                        '{"id":5,"name":"Duke","owner":"Tom"}]',
+                );
+                return next(Serverless_Events["api Gateway Proxy Event"]);
+            })
+            .then(({ statusCode, body }) => {
+                expect(statusCode).toBe(200);
+                expect(body).toBe(
+                    '[{"id":1,"name":"Bubbles","tag":"fish"},' +
+                        '{"id":2,"name":"Jax","tag":"cat"}]',
+                );
+                console.log = cLog.bind(console);
+                done();
+            });
+    });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,82 +1,82 @@
 "use strict";
 import {
-  APIGatewayProxyEvent,
-  APIGatewayProxyEventV2,
-  APIGatewayProxyResult,
-  Context,
+    APIGatewayProxyEvent,
+    APIGatewayProxyEventV2,
+    APIGatewayProxyResult,
+    Context,
 } from "aws-lambda";
 
 type ExecutionContent = {
-  statusCode: Number | String;
-  resBody: Object | String;
-  startedAt: Date;
-  finishedAt: Date;
+    statusCode: Number | String;
+    resBody: Object | String;
+    startedAt: Date;
+    finishedAt: Date;
 };
 
 function log(
-  event: APIGatewayProxyEvent | APIGatewayProxyEventV2,
-  executionContent: ExecutionContent,
+    event: APIGatewayProxyEvent | APIGatewayProxyEventV2,
+    executionContent: ExecutionContent,
 ) {
-  const date1_ms = executionContent?.startedAt?.getTime?.();
-  const date2_ms = executionContent?.finishedAt?.getTime?.();
+    const date1_ms = executionContent?.startedAt?.getTime?.();
+    const date2_ms = executionContent?.finishedAt?.getTime?.();
 
-  const executionTime = Number.isFinite(date2_ms - date1_ms)
-    ? date2_ms - date1_ms
-    : 0;
+    const executionTime = Number.isFinite(date2_ms - date1_ms)
+        ? date2_ms - date1_ms
+        : 0;
 
-  const logExt = {
-    event,
-    response: {
-      statusCode: executionContent?.statusCode,
-      body: executionContent?.resBody,
-    },
-    executionTime,
-    observations: executionContent?.observations,
-  };
+    const logExt = {
+        event,
+        response: {
+            statusCode: executionContent?.statusCode,
+            body: executionContent?.resBody,
+        },
+        executionTime,
+        observations: executionContent?.observations,
+    };
 
-  console.log(
-    "firetail:log-ext:" +
-      Buffer.from(JSON.stringify(logExt)).toString("base64"),
-  );
+    console.log(
+        "firetail:log-ext:" +
+            Buffer.from(JSON.stringify(logExt)).toString("base64"),
+    );
 }
 
 function wrap(next: Function) {
-  return (
-    event: APIGatewayProxyEvent | APIGatewayProxyEventV2,
-    context: Context,
-  ) => {
-    const startedAt = new Date();
-    const observations = [];
+    return (
+        event: APIGatewayProxyEvent | APIGatewayProxyEventV2,
+        context: Context,
+    ) => {
+        const startedAt = new Date();
+        const observations = [];
 
-    let workInProgress;
-    if (typeof next === "function" && typeof next.then === "function") {
-      workInProgress = next(event, context, () => {});
-    } else {
-      workInProgress = Promise.resolve(next(event, context, () => {}));
-      observations.push({
-        type: "firetail.configuration.synchronous.handler.detected",
-        title: "The wrapper has been called with a synchronous function"
-        details: {
-          integration: "JS Lambda Wrapper",
-          version: "1.0.1"
+        let workInProgress;
+        if (typeof next === "function" && typeof next.then === "function") {
+            workInProgress = next(event, context, () => {});
+        } else {
+            workInProgress = Promise.resolve(next(event, context, () => {}));
+            observations.push({
+                type: "firetail.configuration.synchronous.handler.detected",
+                title: "The wrapper has been called with a synchronous function"
+                details: {
+                    integration: "JS Lambda Wrapper",
+                    version: "1.0.1"
+                }
+            });
         }
-      });
-    }
 
-    return workInProgress.then((result: APIGatewayProxyResult) => {
-      const { body, statusCode } = result;
-      const finishedAt = new Date();
+        return workInProgress.then((result: APIGatewayProxyResult) => {
+            const { body, statusCode } = result;
+            const finishedAt = new Date();
 
-      log(event, {
-        statusCode: statusCode || 200,
-        resBody: body,
-        startedAt,
-        finishedAt,
-        observations,
-      });
-      return result;
-    });
-  };
+            log(event, {
+                statusCode: statusCode || 200,
+                resBody: body,
+                startedAt,
+                finishedAt,
+                observations,
+            });
+            return result;
+        });
+    };
 }
 
 export = wrap;
